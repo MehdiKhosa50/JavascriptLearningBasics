b4f5fb6197e6da17ebaf818463c5c54a73349984
Muhammad Muntazir Mehdi <MehdiKhosa50>
2024-05-26 21:08:01 +0500

    add,remove eventListner

--- LearningBasics/Events/script.js
+++ LearningBasics/Events/script.js
@@ -1,73 +1,25 @@
 let p = document.querySelector("p");
 let h1 = document.querySelector("#myID");
-h1.onclick = (evt) => {
-    console.log("You see the Magic!");
-    console.log(evt);
-}
-p.onmouseover = (evt) => {
+
+p.addEventListener("mouseover", (evt) => {
     console.log("You saw it!");
     console.log(evt);
+});
+h1.addEventListener("click", (evt) => {
+    console.log("You saw it! 01");
+    console.log(evt);
+});
+h1.addEventListener("click", (evt) => {
+    console.log("You saw it! 02");
+    console.log(evt);
+});
+const handler03 = () => {
+    console.log("You don't saw it! Because it removed.");
 }
+h1.addEventListener("click",handler03)
+h1.addEventListener("click", (evt) => {
+    console.log("You saw it! 04");
+    console.log(evt);
+});
 
-
-
-
-
-
-// let newBtn = document.createElement("h1");
-// newBtn.innerText = "CLICK ME!";
-// newBtn.style.backgroundColor = "red";
-// newBtn.style.color = "white";
-// document.querySelector("body").prepend(newBtn);
-// document.querySelector("body").append(newBtn);
-// document.querySelector("body").before(newBtn);
-// document.querySelector("body").after(newBtn);
-//newBtn.remove();
-
-
-
-
-
-
-// let h4 = document.querySelector("h2");
-// console.dir(h4.innerText);
-// h4.innerText = h4.innerText + " From MehdiKhosa";
-
-// let boxes = document.querySelectorAll(".boxes");
-// console.log(boxes);
-// let i=1;
-// for(box of boxes){
-//     box.innerText = `NewBoxes ${i}`;
-//     i++
-// }
-
-
-// console.log(boxes[0].innerText);
-// console.log(boxes[1].innerText);
-// console.log(boxes[2].innerText);
-// console.log(boxes[3].innerText);
-
-// boxes[0].innerText = "NewBoxes01";
-// boxes[1].innerText = "NewBoxes02";
-// boxes[2].innerText = "NewBoxes03";
-// boxes[3].innerText = "NewBoxes04";
-
-
-// let myId = document.querySelector("#intro");
-// console.log(myId);
-// console.dir(myId);
-// let myClass = document.querySelector(".myClass");
-// console.log(myClass);
-// console.dir(myClass);
-// console.log("From here we are going to use InnerHTMl");
-// console.log(document.body.innerHTML);
-// console.log("From here we are going to start InnerText");
-// console.log(document.body.innerText);
-// console.log("From here we are going to start textConetnt to show private tags");
-// console.log(document.body.textContent);
-// let el = document.body.childNodes[2].innerText = "ABCD";
-// let el1 = document.body.childNodes[2].innerHTMl = "<div></div>";
-// let el2 = document.body.childNodes[2].textContent = "You're Hidden!";
-// console.log(el);
-// console.log(el1);
-// console.log(el2);
+h1.removeEventListener("click",handler03)
